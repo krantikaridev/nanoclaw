e380d38d18fbc043a3439444ce5f8e48a1e61108
Madan Mohan Meena <krantikarideveloper@gmail.com>
2026-04-18 13:45:56 +0530

    Refactor USDT balance retrieval and swap function

--- clean_swap.py
+++ clean_swap.py
@@ -3,7 +3,6 @@ from web3 import Web3
 from dotenv import load_dotenv
 import os
 from datetime import datetime
-
 load_dotenv()
 
 WALLET = "0x6e291a7180bD198d67Eeb792Bb3262324D3e64AA"
@@ -16,11 +15,11 @@ ROUTER = "0xE592427A0AEce92De3Edee1F18E0157C05861564"
 w3 = Web3(Web3.HTTPProvider(RPC))
 print("RPC connected:", w3.is_connected())
 
-usdt_bal = w3.eth.contract(address=USDT, abi=[{"constant":True,"inputs":[{"name":"_owner","type":"address"}],"name":"balanceOf","outputs":[{"name":"balance","type":"uint256"}],"payable":False,"stateMutability":"view","type":"function"}]).functions.balanceOf(WALLET).call() / 10**6
-print("Real USDT balance:", usdt_bal)
+bal = w3.eth.contract(address=USDT, abi=[{"constant":True,"inputs":[{"name":"_owner","type":"address"}],"name":"balanceOf","outputs":[{"name":"balance","type":"uint256"}],"payable":False,"stateMutability":"view","type":"function"}]).functions.balanceOf(WALLET).call() / 10**6
+print("Real USDT balance:", bal)
 
 async def swap():
-    amount = 2_000_000   # 2 USDT
+    amount = 2_000_000
     router = w3.eth.contract(address=ROUTER, abi=[{"inputs":[{"components":[{"name":"tokenIn","type":"address"},{"name":"tokenOut","type":"address"},{"name":"fee","type":"uint24"},{"name":"recipient","type":"address"},{"name":"deadline","type":"uint256"},{"name":"amountIn","type":"uint256"},{"name":"amountOutMinimum","type":"uint256"},{"name":"sqrtPriceLimitX96","type":"uint160"}],"name":"params","type":"tuple"}],"name":"exactInputSingle","outputs":[{"name":"","type":"uint256"}],"stateMutability":"payable","type":"function"}])
 
     params = {
